9e1b598c6e16ff4470a836165e3e622798c2a153
mohamedsharrif <sharifmohamedmahamud@gmail.com>
2024-05-14 16:44:16 +0300

    updated herosection

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -10,7 +10,7 @@ const HomeHero = () => {
     rgb(0 0 0/60%)),url(./images/misc/home.jpeg)`,
       }}
     >
-      <header className="flex w-full justify-between ">
+      <header className="flex w-full justify-between">
         <div className="my-6 mx-4">
           <Link to="/home">
             <svg
@@ -36,13 +36,51 @@ const HomeHero = () => {
             </svg>
           </Link>
         </div>
+
         <div className="my-6 mx-6">
-        <Link to="/login" className="rounded bg-[#e50a14] py-2 px-8 text-white">
+          <Link
+            to="/login"
+            className="rounded bg-[#e50a14] py-2 px-8 text-white"
+          >
             Signin
-        </Link>
+          </Link>
         </div>
-       
       </header>
+      <main className="flex text-white flex-1 flex-col items-center justify-center text-center sm:px-20">
+        <h1 className="max-w-lg text-5xl font-bold ">
+          Unlimited Movies, Tv Shows, and More{" "}
+        </h1>
+        <h2 className="mt-4 mb-8  text-2xl">
+          Watch any where, Cancel any time
+        </h2>
+        <p className="text-lg">
+          Ready to watch? Enter your email to create or restart your membership
+        </p>
+        <div className="m-4 flex flex-col space-y-6 sm:flex-row sm:space-y-0">
+          <input
+            className="flex w-full min-w-[400px] py-3 px-4 bg-black bg-opacity-40 rounded text-white focus:outline-none focus:ring-2 focus:ring-red-500 border border-gray-400 sm:max-w-xs"
+            type="email"
+            placeholder="Enter your email address"
+          />
+          <button className="flex w-full items-center justify-center bg-[#e50914] text-xl sm:py-0 sm:px-8 font-bold py-2 m-2 rounded">
+            Get Started
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
