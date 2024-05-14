a4ef61b9a482baf9bb630ff6429cf1e8c39672c9
mohamedsharrif <sharifmohamedmahamud@gmail.com>
2024-05-14 15:34:16 +0300

    bu ilded our hero components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pajes/Signup";
+import Home from "./pajes/Home";
+import List from "./pajes/List";
+import Login from "./pajes/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pajes/Home.jsx
+++ src/pajes/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomeHero from '../components/HomeHero'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='relative'>
+      <HomeHero />
     </div>
   )
 }
